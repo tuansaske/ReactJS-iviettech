c9626450c219994166aca8afbd544fbf1cc2a749
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-06-26 17:19:46 +0700

    ver 3.0.0 hoc ngay 26-06-2019
    ver 3.0.0 hoc ngay 26-06-2019

---
+++ Thuc Hanh/Bai-tap-hoc-KHOAPHAM/public/main-bai-1-ngay-26-06-2019.js
@@ -0,0 +1,52 @@
+var FormDangNhap = React.createClass({ //phai viet hoa chu bien dau
+    render: function(){
+      return(
+        <h1>aaaaaaaaaa</h1>
+        );
+    }
+});
+
+ReactDOM.render(
+  <div>
+    <FormDangNhap />
+  </div>
+  ,document.getElementById("root"));
+
+
+// const a = (
+//           <div className="wrap-form">
+//           <span className="contact100-form-title">
+//           Đăng nhập
+//           </span>
+//             <form>
+//
+//               <div className="group-input">
+//                 <label className="label-input100" >Tên đăng nhập *</label>
+//                 <input type="text" className="input100" name="user" placeholder="tuanna" />
+//               </div>
+//
+//               <div className="group-input">
+//                 <label className="label-input100" >Mật Khẩu *</label>
+//                 <input type="password" className="input100" name="pass" placeholder="******" />
+//               </div>
+//
+//               <div className="container-contact100-form-btn">
+//                 <button className="contact100-form-btn">
+//                   <span>
+//                   Submit
+//                   <i className="zmdi zmdi-arrow-right m-l-8"></i>
+//                   </span>
+//                 </button>
+//               </div>
+//
+//             </form>
+//           </div>
+//           );
+//     // change code below this line
+// ReactDOM.render(a, document.getElementById('root'));
+
+
+// ReactDOM.render(
+//   <h1> Hello world!</h1>, //para 1 "truyen gi can"
+//   document.getElementById('root') // chỉ định đối tượng để ghi ra
+// );
