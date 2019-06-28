c061ce605fa8be838cc06a8e0027c00c69165ffe
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-06-28 08:33:08 +0700

    ver 4.0.1 hoc buoi toi
    ver 4.0.1 hoc buoi toi

---
+++ Hoc ES6/js/01-let-01.js
@@ -0,0 +1,23 @@
+// console.log("ABC");
+
+/*========BLOCK SCOPE===========*/
+// 
+// for{ //BLOCK FOR
+//   if (true) { //BLOCK IF
+//     while (true) { //BLOCK WHILE
+//
+//     }
+//   }
+// }
+
+/*********LET**********/
+let score = 100;
+let msg = "WIN";
+if(score>50){
+  // let msg = "WIN"; // let khai bao chi goi duoc trong noi ham va phai dat no ra ngoai.
+  console.log(msg);
+}
+console.log(score);
+console.log(msg);
+
+// let sử dụng trong 1 blook scrope
