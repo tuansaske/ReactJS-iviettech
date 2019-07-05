a073a679abeeb8d3f7bed283a22850b27893cf23
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-07-05 16:27:02 +0700

    ver 1.0.1 update
    ver 1.0.1 update

--- Project-01-myWeb-Reactjs/web-ban-hang/src/index.js
+++ Project-01-myWeb-Reactjs/web-ban-hang/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Demo from './demo'
+import Demo from './demo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Demo />, document.getElementById('root'));
