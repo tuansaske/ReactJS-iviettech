36c8e4f43eb258ea2c07b5a0d30ff3509f66480d
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-07-11 23:43:23 +0700

    update new brach cmt

---
+++ Thuc Hanh/Bai-tap-ve-nha-so-3/src/bai-so-1-Calculate/baiSo1.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../bai-so-1/baiSo1.css';
+
+import ButtomNum from '../bai-so-1/componentCalc/buttomKeyPad.js';
+import ScreenResult from '../bai-so-1/componentCalc/screenResult.js';
+
+class Caculator extends React.Component{
+
+
+
+
+  render(){
+    return(
+      <div className="container">
+        <div className="calc-body">
+          <ScreenResult ketquaTinhtoan={this.state.ketquaTinhtoan}/>
+          <ButtomNum suKienBamNut={this.suKienBamNut}/>
+          <Toggle2/>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Caculator;
+
+
+class Toggle2 extends React.Component {
+
+
+  render() {
+    return (
+      <Toggle/>
+    );
+  }
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
---
+++ Thuc Hanh/Bai-tap-ve-nha-so-3/src/bai-so-1-Calculate/component/screenResult.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class ScreenResult extends React.Component{
+
+  render(){
+    let {ketquaTinhtoan} = this.props;
+    return(
+      <div className="calc-screen">
+        <div className="calc-operation">2536 + 419 + </div>
+        <div className="calc-typed">{ketquaTinhtoan}<span id="blinkerId" className="blink-me">_</span></div>
+      </div>
+    )
+  }
+}
+
+export default ScreenResult;
